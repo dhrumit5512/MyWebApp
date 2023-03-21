f2f3fc6a88b01707271732a0b7257a94eb642e6b
dhrumit <dhrumitbarot1@gmail.com>
2023-03-21 15:57:54 -0600

    i added coding in product content for showing book list also added map from json(API).

---
+++ src/Product.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React, { useState } from 'react';
+// import products from './Products.json';
+
+const Product = ({data, addToCart, openCart}) => {
+    const { id, title, pageCount, thumbnailUrl, categories,authors } = data;
+
+    return (
+        <div className='product' key={id}>
+            <img src={thumbnailUrl} />
+            <h3>{title}</h3>
+            <h6>Authore: {authors}</h6>
+            <p>Categories-{categories}</p>
+            <div className='price-wrapper'>
+                <span className='price'>Pages-{pageCount}</span>
+                <button className='product-button' onClick={() => {addToCart(id); openCart()}} >Description</button>
+            </div>
+        </div>
+    )
+}
+
+export default Product;
