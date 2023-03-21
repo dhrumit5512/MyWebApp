5df26cfe0bb9593e27c4545c51487c70d2a1f0d1
dhrumit <dhrumitbarot1@gmail.com>
2023-03-21 12:56:27 -0600

    here in app.js i removed some non important code and added return function for header.

--- src/App.js
+++ src/App.js
@@ -1,57 +1,20 @@
 import React from 'react';
 import { useState } from 'react';
+import Header from './Header';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState("");
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
-    setNewTodo("");
-  };
-  const handleToggleCompleted = (id) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, completed: !todo.completed };
-        } else {
-          return todo;
-        }
-      })
-    );
-  };
-  const handleDeleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
 
- return (
-  <div>
-    <form onSubmit={handleSubmit}>
-    <input
-        type="text"
-        value={newTodo}
-        onChange={(event) => setNewTodo(event.target.value)}
-      />
-      <button type="submit">Add To Do</button>
-      <ul>
-  {todos.map((todo) => (
-    <li key={todo.id}>
-      <input
-        type="checkbox"
-        checked={todo.completed}
-        onChange={() => handleToggleCompleted(todo.id)}
-      />
-      <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-        {todo.text}
-      </span>
-      <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
-    </li>
-  ))}
-</ul>
-  </form>
-  </div>
-);
- }
+  const [showCart, setShowCart] = useState(false);
+  const openCart = () => {
+    setShowCart(true);
+  }
+
+
+  return (
+    <>
+      <Header openCart={openCart} />
+    </>
+  );
+}
 
 export default App;
-
