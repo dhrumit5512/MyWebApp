15e60cb85a05d97c6ef616e2fbfd410c6af63663
dhrumit <dhrumitbarot1@gmail.com>
2023-03-21 15:43:31 -0600

    adding code for shop to show sidebar.

---
+++ src/Shop.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React, { useState } from 'react';
+import Product from './Product'
+
+const Shop = ({data, addToCart, openCart}) => {
+
+    return (
+        <div className='product-grid'>
+            {
+                data.jackets.map((product) => {
+                    return (
+                        <Product data={product} addToCart={addToCart} openCart={openCart}/>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Shop;
