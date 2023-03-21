9ab98876beea56b9564ef4b5f602cab565e94abd
dhrumit <dhrumitbarot1@gmail.com>
2023-03-21 15:36:42 -0600

    here i have start with footer and desige it.

---
+++ src/Footer.js
@@ -0,0 +1,42 @@
+import './App.css';
+
+export default function Footer() {
+
+    return (
+        <footer>
+            <div>
+                <h4>Social App</h4>
+                <ul>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Facebook</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>For Help</h4>
+                <ul>
+                    <li><a href="#">chat with Us </a></li>
+                    <li><a href="#">call us </a></li>
+                    <li><a href="#">Email us </a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>About Us</h4>
+                <ul>
+                    <li><a href="#">Statement</a></li>
+                    <li><a href="#">Mission</a></li>
+                    <li><a href="#">Mission</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>Where You Can Find Us?</h4>
+                <ul>
+                    <li><a href="#">Canada</a></li>
+                    <li><a href="#">United States</a></li>
+                    <li><a href="#">India</a></li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
