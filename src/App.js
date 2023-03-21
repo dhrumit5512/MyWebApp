d1294980697e549da87ae7bae26ea4cf6af6c1a2
dhrumit <dhrumitbarot1@gmail.com>
2023-03-21 13:48:44 -0600

    here i linked main.js in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import Header from './Header';
+import Main from './Main';
 
 function App() {
 
@@ -8,11 +9,25 @@ function App() {
   const openCart = () => {
     setShowCart(true);
   }
+  const closeCart = () => {
+    setShowCart(false);
+  }
+  const [cartItems, setCartItems] = useState([]);
+  const addToCart = (id) => {
+    const itemToAdd = products.jackets.find((product) => product.id === id);
+    setCartItems([...cartItems, itemToAdd]);
+  };
 
+  const deleteFromCart = (id) => {
+    const updatedCart = cartItems.filter((product) => product.id !==id);
+    setCartItems(updatedCart);
+    console.log(`Product with ID ${id} deleted from cart`);
+  };
 
   return (
     <>
       <Header openCart={openCart} />
+      <Main />
     </>
   );
 }
